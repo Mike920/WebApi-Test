8d764531dcaa0d3caf61eb2fa6b608c54a0a10d4
mike092 <mr@gmail.com>
2015-06-14 14:31:20 +0200

    Initial commit

---
+++ WebApi Server/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebApi_Server.Startup))]
+
+namespace WebApi_Server
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
